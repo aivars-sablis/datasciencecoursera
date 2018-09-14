d90332858ddfcc08b556180224f0a11035beb59b
aivars.sablis <aivars.sablis@gmail.com>
2018-09-14 16:24:13 +0200

    prepare for submission

---
+++ assign-4-cleaning/README.md
@@ -0,0 +1,7 @@
+# Description of script 
+
+run_analysis.md reads data from .zip file either by:
+- downloading required dataset 
+- or if you have required .zip file (Dataset.zip) in your working directory.
+
+# Further descriptions of script can be found in run_analysis.md R Markdown file.
--- assign-4-cleaning/run_analysis.R
+++ assign-4-cleaning/run_analysis.R
@@ -1,44 +1,21 @@
----
-title: "Run analysis"
-output: html_document
----
-
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
-```
-
-## Prepare environment
-
-Load packages
-```r
 packages <- c("data.table", "reshape2")
 sapply(packages, require, character.only = TRUE, quietly = TRUE)
-```
-Set working directory
-```r
+
+#set working directory
 path <- getwd()
-```
 
-
-## Get the data
-```r
+# Get the data
 url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 zip <- "Dataset.zip"
 if (!file.exists(path)) {
   dir.create(path)
 }
 download.file(url, file.path(path, zip))
-```
 
-## Read the files, label (Step 4) and merge training and test sets (Step 1)
-
-Load column names
-```r
+# Load column names
 f <- "UCI HAR Dataset/features.txt"
 col_names <- read.table(unz(file.path(path, zip), f), header = FALSE)
-```
-Load all data
-```r
+
 f <- "UCI HAR Dataset/test/subject_test.txt"
 test_subject <- read.table(unz(file.path(path, zip), f), col.names = "subject", header = FALSE)
 f <- "UCI HAR Dataset/test/y_test.txt"
@@ -46,13 +23,9 @@ test_activity <- read.table(unz(file.path(path, zip), f), col.names = "activity"
 f <- "UCI HAR Dataset/test/X_test.txt"
 # Appropriately labels the data set with descriptive variable names.
 test_data <- read.table(unz(file.path(path, zip), f), col.names = col_names[["V2"]], header = FALSE)
-```
-Merge subjects, activity and data together
-```r
+# Merge subjects, activity and data together
 test <- cbind(test_subject, test_activity, test_data)
-```
 
-```r
 f <- "UCI HAR Dataset/train/subject_train.txt"
 train_subject <- read.table(unz(file.path(path, zip), f), col.names = "subject", header = FALSE)
 f <- "UCI HAR Dataset/train/y_train.txt"
@@ -60,55 +33,41 @@ train_activity <- read.table(unz(file.path(path, zip), f), col.names = "activity
 f <- "UCI HAR Dataset/train/X_train.txt"
 # Appropriately labels the data set with descriptive variable names.
 train_data <- read.table(unz(file.path(path, zip), f), col.names = col_names[["V2"]], header = FALSE)
-```
-Merge subjects, activity and data together
-```r
+# Merge subjects, activity and data together
 train <- cbind(train_subject, train_activity, train_data)
-```
 
-Merges the training and the test sets to create one data set.
-```r
+# Merges the training and the test sets to create one data set.
 data <- rbind(train, test)
-```
 
-```r
 #remove temp variables from memory
 rm(train, train_data, test, test_data)
-```
 
-## Extract measurements (Step 2).
-
-Extracts only the measurements on the mean and standard deviation for each measurement
-```r
+# Extracts only the measurements on the mean and standard deviation for each measurement.
 extract_col_names <- as.integer(grep("mean\\(\\)|std\\(\\)", col_names[["V2"]]))
 data <- select(data, extract_col_names)
-```
 
-## Ddescriptive names (Step 3)
+# Uses descriptive activity names to name the activities in the data set
 
-Uses descriptive activity names to name the activities in the data set
-```r
 f <- "UCI HAR Dataset/activity_labels.txt"
 activity_labels_table <- read.table(unz(file.path(path, zip), f), header = FALSE)
 activity_labels <- as.vector(activity_labels_table[["V2"]])
 data <- data %>% mutate(activity=cut(activity, breaks=c(0, 1, 2, 3, 4, 5, 6), 
                                      labels = activity_labels))
-```
 
-## Tidy data set (Step 5)
-From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+# From the data set in step 4, creates a second, independent tidy data set 
+# with the average of each variable for each activity and each subject.
 
-```r
 melted_data <- melt(extracted_data, id = c("subject", "activity"))
 
 tidy <- dcast(melted_data, subject+activity ~ variable, mean)
-```
-write the tidy data set to a file
-```r
-write.csv(tidy, "tidy.csv", row.names=FALSE)
-```
 
-remove data
-```r
+# write the tidy data set to a file
+write.csv(tidy, "tidy.csv", row.names=FALSE)
+
+# remove data
 rm(data, melted_data)
-```
+
+#average_data <- extracted_data %>% group_by(activity, subject) 
+#summarize(average_data, avg = mean())
+#features <- grep("^f[a-zA-Z]+\\-mean\\(\\)", col_names[["V2"]])
+#col_names[features,]
